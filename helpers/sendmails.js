facc7c957dea74d480a70709d469ee4557ab4f0c
Iqbal <asifmai@hotmail.com>
2020-12-20 16:22:21 +0500

    'update'

--- helpers/sendmails.js
+++ helpers/sendmails.js
@@ -1,12 +1,17 @@
 const Campaign = require('../models/Campaign');
+// @todo remove connectDb and dotenv lines
 const connectDb = require('../config/db');
 require('dotenv').config();
 
 module.exports.run = () => new Promise(async (resolve, reject) => {
   try {
     await connectDb();
-    const campaigns = await Campaign.find();
+    const campaigns = await Campaign.find().populate('replies server');
     console.log(campaigns);
+    
+    for (let i = 0; i < campaigns.length; i++) {
+      
+    }
     
     resolve(true);
   } catch (error) {
