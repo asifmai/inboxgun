1fb17f09ad2efc365457873fb1182daacb90d4eb
Iqbal <asifmai@hotmail.com>
2020-12-20 16:24:26 +0500

    'update'

--- helpers/sendmails.js
+++ helpers/sendmails.js
@@ -1,13 +1,19 @@
+const fs = require('fs');
+const path = require('path');
 const Campaign = require('../models/Campaign');
+let accounts = [];
 // @todo remove connectDb and dotenv lines
 const connectDb = require('../config/db');
 require('dotenv').config();
 
 module.exports.run = () => new Promise(async (resolve, reject) => {
   try {
+    const accountsPath = path.resolve(__dirname, '../config/accounts.json');
+    accounts = JSON.parse(fs.readFileSync(accountsPath));
     await connectDb();
     const campaigns = await Campaign.find().populate('replies server');
     console.log(campaigns);
+    console.log(accounts.length);
     
     for (let i = 0; i < campaigns.length; i++) {
       
