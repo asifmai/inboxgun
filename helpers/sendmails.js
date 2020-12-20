a13094bdec818dc0a04da9eaf36a669c74a59434
Iqbal <asifmai@hotmail.com>
2020-12-20 16:29:00 +0500

    'update'

--- helpers/sendmails.js
+++ helpers/sendmails.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 const Campaign = require('../models/Campaign');
-let accounts = [];
-// @todo remove connectDb and dotenv lines
+
+// @todo remove below lines
 const Reply = require('../models/Reply');
 const Setting = require('../models/Setting');
 const connectDb = require('../config/db');
@@ -10,20 +10,36 @@ require('dotenv').config();
 
 module.exports.run = () => new Promise(async (resolve, reject) => {
   try {
-    const accountsPath = path.resolve(__dirname, '../config/accounts.json');
-    accounts = JSON.parse(fs.readFileSync(accountsPath));
     await connectDb();
     const campaigns = await Campaign.find().populate('replies server');
     console.log(campaigns);
-    console.log(accounts.length);
     
     for (let i = 0; i < campaigns.length; i++) {
-      
+      const accounts = await fetchAccounts(campaigns[i].numberOfEmails);
+      console.log(accounts);
     }
     
     resolve(true);
   } catch (error) {
     console.log(`Send Mails Error: ${error.stack}`);
+    reject(error);
+  }
+});
+
+const fetchAccounts = (numberOfAccounts) => new Promise(async (resolve, reject) => {
+  try {
+    const accountsPath = path.resolve(__dirname, '../config/accounts.json');
+    const allAccounts = JSON.parse(fs.readFileSync(accountsPath));
+    const accounts = [];
+
+    for (let i = 0; i < numberOfAccounts; i++) {
+      const rndIdx = Math.floor(Math.random() * numberOfAccounts);
+      accounts.push(allAccounts[rndIdx].email);
+    }
+
+    resolve(accounts);
+  } catch (error) {
+    console.log(`fetchAccounts Error: ${error.stack}`);
     reject(error);
   }
 });
